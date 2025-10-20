bbbbac25f4319bf0964f7e0357266fdec4f954d2
Vitor Andrade Valles <161478753+Vitor-Valles@users.noreply.github.com>
2025-10-20 20:02:54 -0300

    Add files via upload

---
+++ script1.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+#Echo imprime a mensangem, neste caso ela será salva no arquivo output.txt
+echo "Esse e meu primeiro script em BASH" > output.txt
+#Aqui temos o exemplo de um "Quebra de linha" sendo usada. Veja que temos o >> no lugar do >, isso demonstra que o texto está sendo acresentado e não atribuido
+echo "" >> output.txt
+#Aqui temos apenas uma linha para ficar esteticamnte mais agradavel
+echo "##################################" >> output.txt
+
+#O resultado esperado será´:
+#Esse e meu primeiro script em BASH
+#
+###################################
+
